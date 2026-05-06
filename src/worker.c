603cda1fe80761f1330bec86d0aa118ecf5699b2
Conghui Chen <conghuic@qti.qualcomm.com>
2026-05-06 16:49:12 -0700

    epoll: do not store pointers in epoll_event.data
    Replace epoll_event.data.ptr usage with data.fd and fd lookup to avoid
    use-after-free when epoll_wait returns stale events after EPOLL_CTL_DEL.
    
    Change-Id: I4b98525612bb45a62cf5f5f26ca14be2d1a001fc
    Signed-off-by: Conghui Chen <conghuic@qti.qualcomm.com>

--- src/worker.c
+++ src/worker.c
@@ -11,6 +11,7 @@
 #include <unistd.h>
 #include <vhost_user.h>
 #include <errno.h>
+#include <string.h>
 #include "worker.h"
 #include "log.h"
 
@@ -28,12 +29,23 @@ struct fd_info {
 static LIST_HEAD(listhead, fd_info) fdlist_head;
 static pthread_mutex_t fdmutex = PTHREAD_MUTEX_INITIALIZER;
 
+static struct fd_info *find_fd_info_by_fd(int fd)
+{
+    struct fd_info *fdi;
+    LIST_FOREACH(fdi, &fdlist_head, fdinfo_list) {
+        if (fdi->fd == fd)
+            return fdi;
+    }
+    return NULL;
+}
+
 static void *worker_thr(void *arg)
 {
     struct epoll_event eventlist[20];
     struct fd_info *fi;
     int fd;
-    int ret = 0, exit = 0, i, len;
+    int ret = 0, exit = 0, i;
+    int len;
     char buf[20];
 
     for (;;) {
@@ -49,22 +61,28 @@ static void *worker_thr(void *arg)
         }
         pthread_mutex_lock(&fdmutex);
         for (i = 0; i < ret; i++) {
-            fi = eventlist[i].data.ptr;
-            if (fi)
-                fd = fi->fd;
-            else
-                fd = eventlist[i].data.fd;
+            fd = eventlist[i].data.fd;
+
             if (fd == pipefd[0]) {
                 pr_info("recieve pipe event, need to exit\n");
                 exit = 1;
-            } else {
-                do {
-                    len = read(fd, buf, sizeof(buf));
-                } while (len == 20);
-                if (fi && fi->cb && fi->data) {  // Check if fi->data is not NULL
-                    pr_debug("handle virtio request fd = %d\n", fi->fd);
-                    (fi->cb)(fi->data);
-                }
+                continue;
+            }
+
+            /* Lookup current fd_info by fd; if not found => stale event */
+            fi = find_fd_info_by_fd(fd);
+            if (!fi) {
+                pr_debug("stale epoll event: fd=%d not in list, skipping\n", fd);
+                continue;
+            }
+
+            do {
+                len = read(fd, buf, sizeof(buf));
+            } while (len == (int)sizeof(buf));
+
+            if (fi->cb && fi->data) {
+                pr_debug("handle virtio request fd = %d\n", fi->fd);
+                (fi->cb)(fi->data);
             }
         }
         pthread_mutex_unlock(&fdmutex);
@@ -88,16 +106,20 @@ int start_watch_on_fd(int fd, kick_handler cb, void *data)
 
     if (epoll_fd == 0) {
         epoll_fd = epoll_create1(0);
+        if (epoll_fd < 0)
+            return -1;
+
         if (pipe2(pipefd, O_NONBLOCK) < 0)
             return -1;
+
         event_pipe.events = EPOLLIN;
         event_pipe.data.fd = pipefd[0];
-        event_pipe.data.ptr = NULL;
         epoll_ctl(epoll_fd, EPOLL_CTL_ADD, pipefd[0], &event_pipe);
 
         pthread_create(&recv_pid, NULL, worker_thr, NULL);
         pr_info("start worker thread \n");
     }
+
     fi = calloc(1, sizeof(struct fd_info));
     if (!fi) {
         pr_err("failed to calloc the fd_info\n");
@@ -106,16 +128,24 @@ int start_watch_on_fd(int fd, kick_handler cb, void *data)
     fi->cb = cb;
     fi->data = data;
     fi->fd = fd;
+
+    /* Insert into list first (under lock), so worker can always find it by fd */
+    pthread_mutex_lock(&fdmutex);
+    LIST_INSERT_HEAD(&fdlist_head, fi, fdinfo_list);
+    pthread_mutex_unlock(&fdmutex);
+
+    /* Register fd with epoll using data.fd only */
     event.events = EPOLLIN;
     event.data.fd = fd;
-    event.data.ptr = fi;
-    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &event);
-
-    pthread_mutex_lock(&fdmutex);
-
-    LIST_INSERT_HEAD(&fdlist_head, fi, fdinfo_list);
-
-    pthread_mutex_unlock(&fdmutex);
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &event) < 0) {
+        pr_err("failed to add fd to epoll: fd=%d err=%s(%d)\n", fd, strerror(errno), errno);
+        /* rollback list insert */
+        pthread_mutex_lock(&fdmutex);
+        LIST_REMOVE(fi, fdinfo_list);
+        pthread_mutex_unlock(&fdmutex);
+        free(fi);
+        return -1;
+    }
 
     return 0;
 }
@@ -128,30 +158,30 @@ void stop_watch_on_fd(int fd)
         return;
 
     pr_debug("stop watching on fd = %d \n", fd);
-    if (epoll_fd) {
-        if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL) < 0) {
-            pr_err("Note! failed to del fd from epoll.");
-            return;
-        }
 
-        pthread_mutex_lock(&fdmutex);
+    if (!epoll_fd)
+        return;
 
-        LIST_FOREACH(fdi, &fdlist_head, fdinfo_list) {
-            if (fdi->fd == fd) {
-                LIST_REMOVE(fdi, fdinfo_list);
-                free(fdi);
-                break;
-            }
-        }
-
-        pthread_mutex_unlock(&fdmutex);
+    /* Remove fd from epoll interest list */
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL) < 0) {
+        pr_err("Note! failed to del fd from epoll: fd=%d err=%s(%d)\n", fd, strerror(errno), errno);
+        /* Continue to try to cleanup list anyway */
     }
+
+    pthread_mutex_lock(&fdmutex);
+    fdi = find_fd_info_by_fd(fd);
+    if (fdi) {
+        LIST_REMOVE(fdi, fdinfo_list);
+        free(fdi);
+    }
+    pthread_mutex_unlock(&fdmutex);
 }
 
 void kill_worker(void)
 {
     char buf = 0;
     void *jval;
+    struct fd_info *fdi;
 
     if (epoll_fd && pipefd[1]) {
         (void)write(pipefd[1], &buf, 1);
@@ -161,4 +191,13 @@ void kill_worker(void)
         epoll_fd = 0;
         pr_info("stop worker thread..... \n");
     }
+
+    /* Free any remaining fd_info nodes (in case stop_watch_on_fd wasn't called) */
+    pthread_mutex_lock(&fdmutex);
+    while (!LIST_EMPTY(&fdlist_head)) {
+        fdi = LIST_FIRST(&fdlist_head);
+        LIST_REMOVE(fdi, fdinfo_list);
+        free(fdi);
+    }
+    pthread_mutex_unlock(&fdmutex);
 }
